3638b01561a838694afeeacc929761eae77a46f5
Shimi Zacken <shimizacken@gmail.com>
2017-03-26 20:46:59 +0300

    react integrated

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import jQuery from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MyComponent extends React.Component {
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
 
-}
-
-
-jQuery('h1').text('Hello dev web pack!!!');
+//jQuery('h1').text('Hello dev web pack!!!');
