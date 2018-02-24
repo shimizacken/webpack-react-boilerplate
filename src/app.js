5bdf0fd2718ee8b72d2ec19c4908e32f8caa0baf
Shimi Zacken <shimizacken@gmail.com>
2018-02-24 21:28:24 +0200

    MainWrapper added

--- src/app.js
+++ src/app.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ToolBar from './components/toolbar';
+import MainWrapper from './components/mainWrapper';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import './styles/main.css';
-import './styles/second.css';
-import './styles/controls.scss';
-
-const App = ({name}) => (
-  <div>
-    <ToolBar />
-    <div>
-      {`Hi ${name}`}
-      </div>
-    </div>
- );
+import './styles/main.scss';
 
 ReactDOM.render(
-  <App name='shimi!' />,
+  <MainWrapper />,
   document.getElementById('root')
 );
---
+++ src/components/mainWrapper.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class MainWrapper extends Component {
+
+    render() {
+
+        return(
+            <div className='main-wrapper'>
+            </div>
+        );
+    }
+}
